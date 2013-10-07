799d9efae81a473567e9a4b6763d28c9408c676b
Simon Walker <simon@stwalkerster.co.uk>
2013-10-07 22:30:45 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 
 rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
 
+php genconfig.php > overviewerconfig
+
 overviewer.py --config=overviewerconfig
