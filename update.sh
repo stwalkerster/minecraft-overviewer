26f4db5b9993f365ff87da47bce23cbcb9a6d589
Simon Walker <simon@stwalkerster.co.uk>
2013-10-07 22:38:26 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+wget -nc https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/1.6.4.jar -P ~/.minecraft/versions/1.6.4/
+
 rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
 
 php genconfig.php > overviewerconfig
