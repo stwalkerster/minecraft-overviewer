c85b021376028c077608d83b12d4d4c81c5cdb9a
Simon Walker <simon@stwalkerster.co.uk>
2013-10-08 12:56:50 +0100

    test

--- update.sh
+++ update.sh
@@ -8,11 +8,12 @@ wget -N https://s3-eu-west-1.amazonaws.com/minecraft-worlds/smp/backups.tar
 
 tar -xf 2012-05-02T000009Z.tar.bz2
 tar -xf backups.tar
-tar -xf backups/1.8pre-final.tar.bz2
-tar -xf backups/freebuild-final.tar.bz2
+tar -xf backups/1.8pre-final.tar.bz2 -C beta1.8
+tar -xf backups/freebuild-final.tar.bz2 -C freebuild/
+tar -xf backups/survival-final.tar.bz2 -C survival/
 
 rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
 
 php genconfig.php > overviewerconfig
 
-overviewer.py --config=overviewerconfig
+# overviewer.py --config=overviewerconfig
