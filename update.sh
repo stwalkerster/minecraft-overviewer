16891a4db033e12a37d2671e04d2a7a17fc124bd
Simon Walker <simon@stwalkerster.co.uk>
2013-10-08 12:58:59 +0100

    make directories

--- update.sh
+++ update.sh
@@ -8,8 +8,11 @@ wget -N https://s3-eu-west-1.amazonaws.com/minecraft-worlds/smp/backups.tar
 
 tar -xf 2012-05-02T000009Z.tar.bz2
 tar -xf backups.tar
-tar -xf backups/1.8pre-final.tar.bz2 -C beta1.8
+mkdir -p beta1.8/
+tar -xf backups/1.8pre-final.tar.bz2 -C beta1.8/
+mkdir -p freebuild/
 tar -xf backups/freebuild-final.tar.bz2 -C freebuild/
+mkdir -p survival/
 tar -xf backups/survival-final.tar.bz2 -C survival/
 
 rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
