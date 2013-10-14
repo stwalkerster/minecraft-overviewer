794b6effc302fb29c84e4725db3cec8922b435cb
Simon Walker <github@stwalkerster.co.uk>
2013-10-14 17:58:01 +0100

    don't re-render the map

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 
 #wget -N https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/1.6.4.jar -P ~/.minecraft/versions/1.6.4/
 
-rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
+#rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude lost+found --exclude '*.tar.bz' minecraft@metapod.lon.stwalkerster.net:/mnt/minecraft/ $WORKSPACE/worlds
 
 php genconfig.php > overviewerconfig
 
 overviewer.py --config=overviewerconfig --genpoi
 
-overviewer.py --config=overviewerconfig
+#overviewer.py --config=overviewerconfig
