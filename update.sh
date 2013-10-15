7fecf6c60ab5066fe42187a2c6ebf91f7ebc2fc4
Simon Walker <simon@stwalkerster.co.uk>
2013-10-15 13:40:22 +0100

    swap render and genpoi order

--- update.sh
+++ update.sh
@@ -19,6 +19,6 @@ rsync -avz -e "ssh -i /var/lib/jenkins/.ssh/minecraft.metapod.id_rsa" --exclude 
 
 php genconfig.php > overviewerconfig
 
-overviewer.py --config=overviewerconfig --genpoi
-
 overviewer.py --config=overviewerconfig
+
+overviewer.py --config=overviewerconfig --genpoi
