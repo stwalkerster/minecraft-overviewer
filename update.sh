1f39905946424aaa2a0526f44a10d04393983b0e
Simon Walker <simon@stwalkerster.co.uk>
2013-10-26 18:20:22 +0100

    update mc version

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-wget -N https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/1.6.4.jar -P ~/.minecraft/versions/1.6.4/
+wget -N https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/1.7.2.jar -P ~/.minecraft/versions/1.7.2/
 
 wget -N https://s3-eu-west-1.amazonaws.com/minecraft-worlds/smp/mc1/2012-05-02T000009Z.tar.bz2
 
