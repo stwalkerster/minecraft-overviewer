134e7623443411d8bf5e01b789378c64f278a8d5
Simon Walker <simon@stwalkerster.co.uk>
2014-06-04 14:06:28 +0100

    bump client version

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-wget -N https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/1.7.2.jar -P ~/.minecraft/versions/1.7.2/
+wget -N https://s3.amazonaws.com/Minecraft.Download/versions/1.7.9/1.7.9.jar -P ~/.minecraft/versions/1.7.9/
 
 wget -N https://s3-eu-west-1.amazonaws.com/minecraft-worlds/smp/mc1/2012-05-02T000009Z.tar.bz2
 
