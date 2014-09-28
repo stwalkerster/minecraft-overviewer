0fbbf7ad6cb8dd27c6b1b502f801bf686e3dc246
Simon Walker <simon@stwalkerster.co.uk>
2014-09-28 13:50:39 +0100

    Delete abort.sh

--- abort.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh jenkins@metapod.lon.stwalkerster.net 'echo '"'"'tellraw @a {"text":"[Jenkins: Build aborted by '$1'.]","color":"gray","italic":true}'"'"' > /mnt/minecraft/minecraft-cowgate.fifo'
