5f36f3ed90fc4825fb513765397a6706efcf1000
Simon Walker <github@stwalkerster.co.uk>
2015-06-23 21:33:51 +0100

    undo refactoring

--- genconfig.php
+++ genconfig.php
@@ -1,10 +1,3 @@
-def pointOfInterestFactory(poi):
-    return "\n".join([
-        poi['Text2'], 
-        poi['Text3'], 
-        poi['Text4'], 
-        "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
-
 def townFilter(poi):
     if poi['id'] == 'Town':
         return poi['name']
@@ -20,57 +13,57 @@ def horseFilter(poi):
 def underbellyFilter(poi):
     if poi['id'] == 'Sign':
         if "Underbelly" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def witchHutSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Witch Hut" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def templeSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Temple" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def houseSignFilter(poi):
     if poi['id'] == 'Sign':
         if "House" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def townSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Town" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def portalSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Portal" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def pointOfInterestSignFilter(poi):
     if poi['id'] == 'Sign':
         if "POI" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
             
 def trainSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Station" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
             
 def dockSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Dock" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 			
 def canalSignFilter(poi):
     if poi['id'] == 'Sign':
         if "Canal" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 def netherFastTransportSignFilter(poi):
     if poi['id'] == 'Sign':
         if "NFT" in poi['Text1']:
-            return pointOfInterestFactory(poi)
+            return "\n".join([poi['Text2'], poi['Text3'], poi['Text4'], "(" + ",".join([str(poi['x']), str(poi['y']), str(poi['z'])]) + ")"])
 
 outputdir = "maps"
 customwebassets = "assets"
