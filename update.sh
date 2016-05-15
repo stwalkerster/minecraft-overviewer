e08e5466c357ad514aa39ef34166fc0d5f9840d1
Simon Walker <simon@stwalkerster.co.uk>
2016-05-15 23:57:51 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 mkdir -p maps
+mkdir -p worlds
 
 #Initiating new render job
 ssh jenkins@metapod.lon.stwalkerster.net 'echo '"'"'tellraw @a {"text":"[Jenkins: Initiating new render job]","color":"gray","italic":true}'"'"' > /mnt/minecraft/minecraft-cowgate.fifo'
