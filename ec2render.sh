80181457775154663b8b430c2c9e1f051913e971
Simon Walker <github@stwalkerster.co.uk>
2016-09-28 19:17:04 +0100

    fixes

--- ec2render.sh
+++ ec2render.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -ex
 
 function serverCommand {
-    ssh -p 8022 jenkins@localhost 'echo '"'"$1"'"' > /mnt/minecraft/minecraft-'$BUILD_WORLD_UNIX_NAME'.fifo'
+    ssh -p 8022 -o StrictHostKeyChecking=no jenkins@localhost 'echo '"'"$1"'"' > /mnt/minecraft/minecraft-'$BUILD_WORLD_UNIX_NAME'.fifo'
 }
 
 echo "deb http://overviewer.org/debian ./" | sudo tee /etc/apt/sources.list.d/overviewer.list
@@ -10,9 +10,10 @@ wget -O - http://overviewer.org/debian/overviewer.gpg.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install minecraft-overviewer -qy
 
-
 # open SSH tunnel to metapod
 ssh -Nf -L 8022:metapod:22 -o ExitOnForwardFailure=yes -o ControlPath=~/.ssh/fearow.lon.stwalkerster.net.ctl jenkins@fearow.lon.stwalkerster.net
+
+sudo chmod a+rwx /mnt
 
 mkdir -p /mnt/maps/$BUILD_WORLD_UNIX_NAME
 
@@ -23,7 +24,7 @@ serverCommand 'tellraw @a {"text":"[Jenkins: Waiting for save to complete, then 
 
 sleep 5
 
-rsync -avz -e "ssh -p 8022" --exclude lost+found --exclude '*.tar.bz' minecraft@localhost:/mnt/minecraft/$BUILD_WORLD_UNIX_NAME/ $WORKSPACE/server
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -p 8022" --exclude lost+found --exclude '*.tar.bz' minecraft@localhost:/mnt/minecraft/$BUILD_WORLD_UNIX_NAME/ $WORKSPACE/server
 
 serverCommand 'tellraw @a {"text":"[Jenkins: Transfer complete. Initiating render.]","color":"gray","italic":true}'
 serverCommand 'save-on'
