7513574edcac8dc1d28596760fc6ec7c95d2d77f
Simon Walker <github@stwalkerster.co.uk>
2019-06-07 21:57:23 +0100

    update

--- runPoiUpdate.sh
+++ runPoiUpdate.sh
@@ -1,25 +1,29 @@
 #!/bin/bash -xe
 
+export ServerHost=ninetales
+export RenderHost=growlithe
+export InstanceName=pillage
+
 function serverCommand {
-    ssh minecraft@ninetales.mgmt.scimonshouse.net 'echo '"'"$1"'"' > /opt/minecraft/tantallon/stdin.fifo'
+    ssh minecraft@${ServerHost}.scimonshouse.net 'echo '"'"$1"'"' > /opt/minecraft/'$InstanceName'/stdin.fifo'
 }
 
-serverCommand 'tellraw @a {"text":"[growlithe: Initiating POI update job]","color":"gray","italic":true}'
+serverCommand 'tellraw @a {"text":"['$RenderHost': Initiating POI update job]","color":"gray","italic":true}'
 serverCommand 'save-off'
 serverCommand 'save-all flush'
 
 sleep 5
 
-rsync -avz minecraft@ninetales.mgmt.scimonshouse.net:/opt/minecraft/ /home/minecraft/minecraft-overviewer/worlds/
+rsync -avz minecraft@${ServerHost}.scimonshouse.net:/opt/minecraft/ /home/minecraft/minecraft-overviewer/worlds/
 
 serverCommand 'save-on'
 
-php genconfig.php > overviewerconfig
+php genconfig-${InstanceName}.php > overviewerconfig-${InstanceName}
 
 PYTHONPATH=`pwd`
 
 #nice --adjustment=10 /home/minecraft/overviewer/overviewer.py --config=overviewerconfig --genpoi
-nice --adjustment=10 overviewer.py --config=overviewerconfig --genpoi
+nice --adjustment=10 overviewer.py --config=overviewerconfig-${InstanceName} --genpoi
 
-serverCommand 'tellraw @a {"text":"[growlithe: Update complete.]","color":"gray","italic":true}'
-serverCommand 'tellraw @a {"text":"          https://scimonshouse.net/minecraft/","italic":false,"color":"yellow","clickEvent":{"action":"open_url","value":"https://scimonshouse.net/minecraft/"}}'
+serverCommand 'tellraw @a {"text":"['$RenderHost': Update complete.]","color":"gray","italic":true}'
+serverCommand 'tellraw @a {"text":"          https://scimonshouse.net/mcmaps/'$InstanceName'/","italic":false,"color":"yellow","clickEvent":{"action":"open_url","value":"https://scimonshouse.net/mcmaps/'$Instance$
--- runRender.sh
+++ runRender.sh
@@ -1,26 +1,30 @@
 #!/bin/bash -xe
 
+export ServerHost=ninetales
+export RenderHost=growlithe
+export InstanceName=pillage
+
 function serverCommand {
-    ssh minecraft@ninetales.mgmt.scimonshouse.net 'echo '"'"$1"'"' > /opt/minecraft/tantallon/stdin.fifo'
+    ssh minecraft@${ServerHost}.scimonshouse.net 'echo '"'"$1"'"' > /opt/minecraft/'$InstanceName'/stdin.fifo'
 }
 
-serverCommand 'tellraw @a {"text":"[growlithe: Initiating new render job]","color":"gray","italic":true}'
+serverCommand 'tellraw @a {"text":"['$RenderHost': Initiating new render job]","color":"gray","italic":true}'
 serverCommand 'save-off'
 serverCommand 'save-all'
 
 sleep 5
 
-rsync -avz minecraft@ninetales.mgmt.scimonshouse.net:/opt/minecraft/ /home/minecraft/minecraft-overviewer/worlds/
+rsync -avz minecraft@${ServerHost}.scimonshouse.net:/opt/minecraft/ /home/minecraft/minecraft-overviewer/worlds/
 
 serverCommand 'save-on'
-serverCommand 'tellraw @a {"text":"[growlithe: Clone of world complete. Initiating render.]","color":"gray","italic":true}'
+serverCommand 'tellraw @a {"text":"['$RenderHost': Clone of world complete. Initiating render.]","color":"gray","italic":true}'
 
-php genconfig.php > overviewerconfig
+php genconfig-${InstanceName}.php > overviewerconfig-${InstanceName}
 
 PYTHONPATH=`pwd`
 
-#nice --adjustment=10 /home/minecraft/overviewer/overviewer.py --config=overviewerconfig -p 24
-nice --adjustment=10 /usr/bin/overviewer.py --config=overviewerconfig -p 24
+nice --adjustment=10 /home/minecraft/overviewer/overviewer.py --config=overviewerconfig-${InstanceName} -p 24
+#nice --adjustment=10 /usr/bin/overviewer.py --config=overviewerconfig-${InstanceName} -p 24
 
-serverCommand 'tellraw @a {"text":"[growlithe: Render complete. ]","color":"gray","italic":true}'
-serverCommand 'tellraw @a {"text":"          https://scimonshouse.net/minecraft/","italic":false,"color":"yellow","clickEvent":{"action":"open_url","value":"https://scimonshouse.net/minecraft/"}}'
+serverCommand 'tellraw @a {"text":"['$RenderHost': Render complete. ]","color":"gray","italic":true}'
+serverCommand 'tellraw @a {"text":"          https://scimonshouse.net/mcmaps/'$InstanceName'/","italic":false,"color":"yellow","clickEvent":{"action":"open_url","value":"https://scimonshouse.net/mcmaps/'$InstanceName'/"}}'
